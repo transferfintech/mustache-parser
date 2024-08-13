839b1702f8eaa9ccc35030f92c6ff56bb36eba19
Justin Hileman <justin@justinhileman.info>
2024-08-13 08:31:46 -0400

    getMock -> getMockBuilder.

--- test/Mustache/Test/Functional/HigherOrderSectionsTest.php
+++ test/Mustache/Test/Functional/HigherOrderSectionsTest.php
@@ -75,7 +75,10 @@ class Mustache_Test_Functional_HigherOrderSectionsTest extends Mustache_Test_Fun
 
     public function testPassthroughOptimization()
     {
-        $mustache = $this->getMock('Mustache_Engine', array('loadLambda'));
+        $mustache = $this->getMockBuilder('Mustache_Engine')
+            ->setMethods(array('loadLambda'))
+            ->getMock();
+
         $mustache->expects($this->never())
             ->method('loadLambda');
 
@@ -89,7 +92,10 @@ class Mustache_Test_Functional_HigherOrderSectionsTest extends Mustache_Test_Fun
 
     public function testWithoutPassthroughOptimization()
     {
-        $mustache = $this->getMock('Mustache_Engine', array('loadLambda'));
+        $mustache = $this->getMockBuilder('Mustache_Engine')
+            ->setMethods(array('loadLambda'))
+            ->getMock();
+
         $mustache->expects($this->once())
             ->method('loadLambda')
             ->will($this->returnValue($mustache->loadTemplate('<em>{{ name }}</em>')));
